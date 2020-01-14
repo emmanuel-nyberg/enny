ddf41ff5d6eb65353ab5f5841df00a26d1966482
Emmanuel Nyberg <emmanuel@stickybit.se>
2020-01-14 11:40:46 +0100

    Build parameters for API call
    Not an entire URL.
    Also add auto-formatting.

--- collector.py
+++ collector.py
@@ -11,39 +11,41 @@ import requests
 ALPHA_VANTAGE_API_URL = "https://www.alphavantage.co/query?"
 DATEFORMAT = "%Y-%m-%d"
 TIMEFORMAT = "%H:%M:%S"
-Config = namedtuple('Config', ['url', 'dateformat', 'timeformat', 'ts_key'])
-
-
+Config = namedtuple("Config", ["url", "dateformat", "timeformat", "ts_key"])
 
 
 class Collector:
     """This class will act as a collector, giving access to the Alpha Vantage API.
     Its methods return the recieved JSON."""
+
     def __init__(self, args, config):
         self.args = args
         self.config = config
 
-    def _generate_endpoint(self, symbol):
-        endpoint = "{}function={}&symbol={}{}&outputsize=full&apikey={}".format(
-                self.config.url,
-                "TIME_SERIES_DAILY" if self.args.daily else "TIME_SERIES_INTRADAY",
-                symbol,
-                "&interval=60min" if self.args.hourly else "",
-                self.args.apikey)
-        return endpoint
+    def _generate_parameters(self, symbol):
+        params = {"symbol": symbol, "outputsize": "full", "apikiey": self.args.apikey}
+        if self.args.hourly:
+            params.update({"interval": "60min", "function": "TIME_SERIES_INTRADAY"})
+        elif self.args.daily:
+            params.update({"function": "TIME_SERIES_DAILY"})
+        else:
+            raise Exception("Either hourly or daily should be defined")
+        return params
 
-    def _api_call(self, endpoint):
-        r = requests.request("GET", endpoint)
+    def _api_call(self, params):
+        r = requests.get(self.config.url, params=params)
         if r.status_code == 200:
-            r.json()['Meta Data']['fetched'] = datetime.datetime.now().strftime(self.config.dateformat + self.config.timeformat)
+            r.json()["Meta Data"]["fetched"] = datetime.datetime.now().strftime(
+                self.config.dateformat + self.config.timeformat
+            )
             return r.json()
         else:
-            e = {"Error": endpoint + " returned " + str(r.status_code)}
+            e = {"Error": r.url + " returned " + str(r.status_code)}
             return e
 
     def collect_data(self, symbol):
-        endpoint = self._generate_endpoint(symbol)
-        payload = self._api_call(endpoint)
+        params = self._generate_parameters(symbol)
+        payload = self._api_call(params)
         if re.search("Error.*", str(payload.keys())):
             return payload
         else:
@@ -53,33 +55,46 @@ class Collector:
         """The AlphaVantage API returns ugly, multi-word dictionary keys.
         Make them prettier. Returns a json/dict."""
         if self._validate_payload(payload):
-            payload["metadata"] = payload.pop('Meta Data')
+            payload["metadata"] = payload.pop("Meta Data")
             payload["timeseries"] = payload.pop(self.config.ts_key)
-            return payload 
+            return payload
         else:
             return {"Error": f"Data for {symbol} is not fresh"}
 
     def _validate_payload(self, data):
         """Check if the data is fresh. Returns a boolean."""
-        if re.search(f"{datetime.date.today().strftime(self.config.dateformat)}.*", str(data[self.config.ts_key].keys())):
+        if re.search(
+            f"{datetime.date.today().strftime(self.config.dateformat)}.*",
+            str(data[self.config.ts_key].keys()),
+        ):
             return True
+
 
 def main():
     """Do everythong"""
     parser = argparse.ArgumentParser()
     parser.add_argument("-k", "--apikey", required=True, help="Must use apikey")
-    parser.add_argument("-s", "--symbols", help="Name of file containing the stocks we shall fetch",
-                        default="./NDX")
-    parser.add_argument("-o", "--outfile", help="Name of file to write data to",
-                        default="./stonks.json")
+    parser.add_argument(
+        "-s",
+        "--symbols",
+        help="Name of file containing the stocks we shall fetch",
+        default="./NDX",
+    )
+    parser.add_argument(
+        "-o", "--outfile", help="Name of file to write data to", default="./stonks.json"
+    )
     parser.add_argument("--daily", help="Fetch daily history", action="store_true")
-    parser.add_argument("--hourly", help="Fetch intraday history by the hour", action="store_true")
+    parser.add_argument(
+        "--hourly", help="Fetch intraday history by the hour", action="store_true"
+    )
     args = parser.parse_args()
 
-    config = Config(ALPHA_VANTAGE_API_URL, 
+    config = Config(
+        ALPHA_VANTAGE_API_URL,
         DATEFORMAT,
         TIMEFORMAT,
-        "Time Series " + "(Daily)" if args.daily else "Time Series " + "(60min)")
+        "Time Series " + "(Daily)" if args.daily else "Time Series " + "(60min)",
+    )
 
     av = Collector(args, config)
     with open(args.symbols, "r") as symbols:
@@ -88,7 +103,8 @@ def main():
                 payload = av.collect_data(s.strip())
                 out.write(json.dumps(payload) + "\n")
 
-                time.sleep(13) #Let's be good API users and limit ourselves to 4-5 API calls a minute
+                time.sleep(13)  # Let's limit ourselves to 4-5 API calls a minute
+
 
 if __name__ == "__main__":
     main()
